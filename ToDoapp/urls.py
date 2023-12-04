c8271cbd0de7d364b81dda3207afe387b60421b9
direc <harikrishnareddybandi123@gmail.com>
2023-12-04 19:37:57 +0530

    Initial Commit

--- ToDoapp/urls.py
+++ ToDoapp/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-
 from ToDoapp import views
 
 urlpatterns = [
@@ -12,5 +13,10 @@ urlpatterns = [
     path('home',views.home_fun,name="tasklist"),
     path('add',views.add_task,name='add_task'),
     path('edit/<int:pk>',views.edit_task,name='edit_task'),
-    path('delete/<int:pk>',views.delete_task,name='delete_task')
+    path('delete/<int:pk>',views.delete_task,name='delete_task'),
+
+    path('logout',views.logout_fun,name='logout')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ToDoapp/views.py
+++ ToDoapp/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import render, redirect
@@ -10,7 +11,8 @@ from django.views.decorators.cache import never_cache, cache_control
 from ToDoapp.forms import Taskform
 from ToDoapp.models import Task
 
-
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def regpage(request):
     return render(request,'register.html')
 
@@ -19,25 +21,30 @@ def regpage(request):
 @never_cache
 def regdata(request):
     user = request.POST['user']
+    mail = request.POST['mail']
     password = request.POST['epass']
     cpassword = request.POST['cpass']
-    if User.objects.filter(Q(username=user)).exists():
+    if User.objects.filter(Q(username=user) | Q(email=mail)).exists():
         return render(request,'register.html',{'data':'Username or Email already exists'})
     else:
         if password==cpassword:
-            u1 = User.objects.create_superuser(username=user,password=password)
+            u1 = User.objects.create_superuser(username=user,email=mail,password=password)
             u1.save()
             return redirect('log')
         else:
             return render(request,'register.html',{'data':'Passwords does not match'})
 
 
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def log_fun(request):
     return render(request,'login.html',{'data':''})
 
-
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def login_fun(request):
     user = request.POST['txtuser']
+    mail = request.POST['txtuser']
     pswd = request.POST['txtpass']
     user1 = authenticate(username=user,password=pswd)
     if user1 is not None:
@@ -50,12 +57,18 @@ def login_fun(request):
         return render(request, 'login.html', {'data': 'your username and password are not matching'})
 
 
+@login_required
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def home_fun(request):
     tasks = Task.objects.all()
-    return render(request,'home.html',{'tasks':tasks})
+    count = tasks.filter(complete=False).count()
+    return render(request,'home.html',{'tasks':tasks,'count':count})
 
 
-
+@login_required
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def add_task(request):
     if request.method == 'POST':
         form = Taskform(request.POST)
@@ -67,6 +80,9 @@ def add_task(request):
     return render(request, 'addtask.html', {'form': form})
 
 
+@login_required
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def edit_task(request,pk):
     task = Task.objects.get(pk=pk)
     if request.method == 'POST':
@@ -79,7 +95,15 @@ def edit_task(request,pk):
     return render(request, 'edittask.html', {'form': form, 'task': task})
 
 
+@login_required
+@cache_control(no_cache=True,revalidate=True,nostore=True)
+@never_cache
 def delete_task(request,pk):
     task = Task.objects.get(pk=pk)
     task.delete()
     return redirect('tasklist')
+
+
+def logout_fun(request):
+    logout(request)
+    return redirect('log')
